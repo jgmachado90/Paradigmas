5de20a415d72abdd1edb037f07b2ade0aa7e0059
João <joao@dell.inf.ufsm.br>
2016-07-06 17:05:01 -0300

    enviou

--- Trab Face/Trab_Face/src/trab_face/Trab_Face.java
+++ Trab Face/Trab_Face/src/trab_face/Trab_Face.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package trabface;
+package trab_face;
 
 /**
  *
  * @author joao
  */
-public class Trabface {
+public class Trab_Face {
 
     /**
      * @param args the command line arguments
